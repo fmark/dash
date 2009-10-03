0d7db8c54007a14aa3d2de7d5e473aa51edddbbd
unknown <Olly-Smack-PC@.(none)>
2009-10-03 17:38:59 +1000

    created winforms project for UI

---
+++ Bessie/Bessie/UI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bessie
+{
+    public partial class UI : Form
+    {
+        public UI()
+        {
+            InitializeComponent();
+        }
+
+    }
+}

8873ef41a5c53e18c2a723a613c43abaaced4d02
fmark <fmarkham@gmail.com>
2010-02-19 13:43:58 +1100

    initial animation canvas infrastructure set up

---
+++ Bessie/Bessie/DoubleBufferedPanel.cs
@@ -0,0 +1,15 @@
+
+using System;
+
+namespace Bessie
+{
+	
+	public class DoubleBufferedPanel: System.Windows.Forms.Panel
+	{
+		
+		public DoubleBufferedPanel()
+		{
+			DoubleBuffered = true;
+		}
+	}
+}
